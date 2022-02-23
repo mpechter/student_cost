6e1bc5a8f536d0652b09cf9b38ecee84250b30cc
Mike Pechter <mpechter@gmail.com>
2022-02-23 17:59:02 -0500

    Added function for costs by county

--- student_cost.py
+++ student_cost.py
@@ -22,23 +22,23 @@ def get_cost_per_student():
 
     return df
 
-def add_zero_to_county_num(df, id, cost):
+def add_zero_to_county_num(df, id, measure):
 
     raw_dict = df.to_dict('index')
 
     conum_array = []
-    per_student_cost_array = []
+    measure_array = []
 
     for key in raw_dict:
         conum = raw_dict[key][id]
-        per_student_cost = raw_dict[key][cost]
+        measure_instance = raw_dict[key][measure]
         if len(conum) == 4:
             conum = '0' + conum
         
         conum_array.append(conum)
-        per_student_cost_array.append(per_student_cost)
+        measure_array.append(measure_instance)
 
-    final_dict = {id:conum_array, cost:per_student_cost_array}
+    final_dict = {id:conum_array, measure: measure_array}
 
     df_final = pd.DataFrame.from_dict(final_dict)
 
@@ -52,13 +52,55 @@ def get_cost_per_student_county():
 
     df.rename(columns={"CONUM": "county_num", "PPCSTOT": "per_student_cost"}, inplace = True)
 
-    df = df.astype({"county_num": str})
+    #df = df.astype({"county_num": str})
 
-    df_final = add_zero_to_county_num(df, 'county_num', 'per_student_cost')
+    #df_final = add_zero_to_county_num(df, 'county_num', 'per_student_cost')
 
-    print(df_final.head())
+    return df
 
-get_cost_per_student_county()
+def get_poverty_by_county():
+    
+    df_all = pd.read_csv('poverty_county.CSV')
+
+    df = df_all[['County Code','Poverty Percent, All Ages','Median Household Income']]
+
+    df.rename(columns={'County Code':'county_num','Poverty Percent, All Ages':'poverty_percent','Median Household Income':'median_income'}, inplace = True)
+
+    return df
+
+def merge_by_county():
+    df_cost_per_county = get_cost_per_student_county()
+    df_poverty_by_county = get_poverty_by_county()
+    df_merge = pd.merge(df_cost_per_county, df_poverty_by_county, on='county_num')
+
+    return df_merge
+
+def plot_cost_by_county(df):
+
+    ax = df.plot.scatter(x='poverty_percent', y='per_student_cost', title='Poverty Rate and Cost per Pupil by County')
+
+    plt.xlabel('County Poverty Rate')
+    plt.ylabel('Cost per Student')
+
+    #fmt = '%.0f%%' # Format you want the ticks, e.g. '40%'
+    #yticks = mtick.FormatStrFormatter(fmt)
+    #ax.yaxis.set_major_formatter(yticks)
+
+    #regression(df, 'costIndex', 'amountPerPupil')
+
+    # set ticks here
+
+    range_array = []
+
+    i = 0
+
+    while i<100:
+        range_array.append(i)
+        i = i + 1
+
+    ax.set_xticks(range_array)    #set interval here
+
+    plt.show()
 
 def merge_by_state():
 
@@ -73,7 +115,7 @@ def plot_cost_by_state(df):
     ax = df.plot.scatter(x='costIndex', y='amountPerPupil', title='Cost of Living Index and Cost per Pupil')
 
     plt.xlabel('Cost of Living Index')
-    plt.ylabel('Cost per Pupil')
+    plt.ylabel('Cost per Student')
 
     #fmt = '%.0f%%' # Format you want the ticks, e.g. '40%'
     #yticks = mtick.FormatStrFormatter(fmt)
@@ -102,3 +144,8 @@ def regression(df, x, y):
 
 #df = merge_by_state()
 #plot_cost_by_state(df)
+
+df = merge_by_county()
+
+print(df)
+#plot_cost_by_county(df)
